350153d2a2d72cbd100cbf7fa20f0cd81abcc704
JenisJ <jenis.jesuratnam@hotmail.com>
2021-05-08 21:37:47 -0700

    initial

---
+++ src/main/java/com/jenis/scrapper/webscrapper/WebScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.jenis.scrapper.webscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapperApplication.class, args);
+	}
+
+}
